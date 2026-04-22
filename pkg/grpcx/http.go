d7ccdbbf6a3c6f4ad92031f6ea50b8fbead38389
Saucesamba <arhipartyom@yandex.ru>
2026-04-22 18:56:56 +0300

    rm dubios error mapping1

--- pkg/grpcx/http.go
+++ pkg/grpcx/http.go
@@ -7,15 +7,11 @@ import (
 	httppkg "github.com/go-park-mail-ru/2026_1_VKino/pkg/http"
 )
 
-func HTTPStatusFromGRPC(err error) (int, string) {
-	return errhttpx.DefaultMapper.Map(err)
-}
-
 func WriteError(w http.ResponseWriter, err error) {
 	if err == nil {
 		return
 	}
 
-	statusCode, message := HTTPStatusFromGRPC(err)
+	statusCode, message := errhttpx.DefaultMapper.Map(err)
 	httppkg.ErrResponse(w, statusCode, message)
 }
