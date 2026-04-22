70f0b19830b83d0bc86562dcb01fae2cfdd97540
Saucesamba <arhipartyom@yandex.ru>
2026-04-22 18:56:06 +0300

    rm dubios error mapping

--- pkg/grpcx/http.go
+++ pkg/grpcx/http.go
@@ -1,40 +1,14 @@
 package grpcx
 
 import (
-	"errors"
 	"net/http"
 
+	errhttpx "github.com/go-park-mail-ru/2026_1_VKino/pkg/errmap/httpx"
 	httppkg "github.com/go-park-mail-ru/2026_1_VKino/pkg/http"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func HTTPStatusFromGRPC(err error) (int, string) {
-	if err == nil {
-		return http.StatusOK, ""
-	}
-
-	st, ok := status.FromError(err)
-	if !ok {
-		return http.StatusInternalServerError, "internal server error"
-	}
-
-	switch st.Code() {
-	case codes.InvalidArgument:
-		return http.StatusBadRequest, st.Message()
-	case codes.NotFound:
-		return http.StatusNotFound, st.Message()
-	case codes.AlreadyExists:
-		return http.StatusConflict, st.Message()
-	case codes.Unauthenticated, codes.PermissionDenied:
-		return http.StatusUnauthorized, "unauthorized"
-	case codes.FailedPrecondition:
-		return http.StatusPreconditionFailed, st.Message()
-	case codes.Unavailable:
-		return http.StatusBadGateway, "service unavailable"
-	default:
-		return http.StatusInternalServerError, "internal server error"
-	}
+	return errhttpx.DefaultMapper.Map(err)
 }
 
 func WriteError(w http.ResponseWriter, err error) {
@@ -43,9 +17,5 @@ func WriteError(w http.ResponseWriter, err error) {
 	}
 
 	statusCode, message := HTTPStatusFromGRPC(err)
-	if message == "" {
-		message = errors.New("internal server error").Error()
-	}
-
 	httppkg.ErrResponse(w, statusCode, message)
 }
